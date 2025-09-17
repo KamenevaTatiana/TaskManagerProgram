72132bd5b7b4c28f91c35b8fbdc5d418b77bf5f6
Tatiana Kameneva <tanyas.necessary@gmail.com>
2025-09-17 15:57:46 +0300

    test added

--- src/test/java/ru/netology/manager/TodosTest.java
+++ src/test/java/ru/netology/manager/TodosTest.java
@@ -138,4 +138,29 @@ public class TodosTest {
 
         Assertions.assertArrayEquals(expected, actual);
     }
+
+    @Test
+    public void shouldSearchWhenSomeTasksMatches() {
+        SimpleTask simpleTask = new SimpleTask(5, "Выкатка 3й версии приложения");
+
+        String[] subtasks = { "Молоко", "Яйца", "Хлеб" };
+        Epic epic = new Epic(55, subtasks);
+
+        Meeting meeting = new Meeting(
+                555,
+                "Выкатка 3й версии приложения",
+                "Приложение НетоБанка",
+                "Во вторник после обеда"
+        );
+
+        Todos todos = new Todos();
+
+        todos.add(simpleTask);
+        todos.add(epic);
+        todos.add(meeting);
+
+        Task[] expected = {simpleTask, meeting};
+        Task[] actual = todos.search("Выкатка 3й версии приложения");
+        Assertions.assertArrayEquals(expected, actual);
+    }
 }
